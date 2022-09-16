4858d7ba5e2b46fd43f628751b57fb31e193a3e0
Artyom <olenin.artyom@gmail.com>
2022-09-16 23:58:37 +0300

    xoxo

---
+++ kr11.pas
@@ -0,0 +1,10 @@
+﻿program kr1olenin;
+const
+  x=-2.235;
+  y=-0.823;
+var 
+  a:real;
+begin
+  a:=(power(E,abs(x-y)))+((power(abs(x-y),(x+y)))/(power(sin(y),2)));
+  writeln('a=',a:4:3);
+end.
---
+++ kr12.pas
@@ -0,0 +1,11 @@
+﻿program kr1olenin;
+const
+  x=-2.235;
+  y=-0.823;
+  z=15.221;
+var 
+  b:real;
+begin
+  b:=(((arctan(x)/7)+(6*(arctan(z))/7)))/((power(z,(1/3))-y));
+  writeln('b=',b:4:3);
+end.
